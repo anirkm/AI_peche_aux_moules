c6b54199956639942f61ae97e8c080316a4241c3
KARAMI Anir <ak@revolik.com>
2025-11-29 17:07:17 +0100

    refacto etat du jeu

--- IA/superAI/EtatJeu.java
+++ IA/superAI/EtatJeu.java
@@ -1,33 +1,12 @@
 package superAI;
 
-import java.util.Arrays;
-
 class EtatJeu {
-    enum TypeCase {
-        MUR,
-        SOL,
-        SAUT,
-        TROIS_PAS,
-        MOULE
-    }
+    private final Labyrinthe labyrinthe;
+    private final Joueur[] joueurs;
 
-    final int largeur;
-    final int hauteur;
-    final TypeCase[] type;
-    final int[] valeur;
-    final int nbJoueurs;
-    final int[] joueursX;
-    final int[] joueursY;
-
-    private EtatJeu(int largeur, int hauteur, TypeCase[] type, int[] valeur,
-                    int nbJoueurs, int[] joueursX, int[] joueursY) {
-        this.largeur = largeur;
-        this.hauteur = hauteur;
-        this.type = type;
-        this.valeur = valeur;
-        this.nbJoueurs = nbJoueurs;
-        this.joueursX = joueursX;
-        this.joueursY = joueursY;
+    private EtatJeu(Labyrinthe labyrinthe, Joueur[] joueurs) {
+        this.labyrinthe = labyrinthe;
+        this.joueurs = joueurs;
     }
 
     static EtatJeu depuisMessage(String message) {
@@ -50,59 +29,31 @@ class EtatJeu {
         if (cases.length != largeur * hauteur) {
             throw new IllegalArgumentException("Structure invalide: " + cases.length);
         }
-
-        TypeCase[] type = new TypeCase[cases.length];
-        int[] valeur = new int[cases.length];
-        for (int i = 0; i < cases.length; i++) {
-            String token = cases[i];
-            if ("Mu".equals(token)) {
-                type[i] = TypeCase.MUR;
-            } else if ("So".equals(token)) {
-                type[i] = TypeCase.SOL;
-            } else if ("Bs".equals(token)) {
-                type[i] = TypeCase.SAUT;
-            } else if ("Bp".equals(token)) {
-                type[i] = TypeCase.TROIS_PAS;
-            } else {
-                type[i] = TypeCase.MOULE;
-                valeur[i] = Integer.parseInt(token);
-            }
-        }
+        Labyrinthe labyrinthe = Labyrinthe.depuisTokens(largeur, hauteur, cases);
 
         // Positions des joueurs
         String[] joueurs = parties[2].split("-");
         int nbJoueurs = Integer.parseInt(joueurs[0]);
-        int[] joueursX = new int[nbJoueurs];
-        int[] joueursY = new int[nbJoueurs];
+        Joueur[] liste = new Joueur[nbJoueurs];
         for (int i = 0; i < nbJoueurs; i++) {
             String[] pos = joueurs[i + 1].split(",");
-            joueursX[i] = Integer.parseInt(pos[0]);
-            joueursY[i] = Integer.parseInt(pos[1]);
+            int x = Integer.parseInt(pos[0]);
+            int y = Integer.parseInt(pos[1]);
+            liste[i] = new Joueur(i, x, y);
         }
 
-        return new EtatJeu(largeur, hauteur, type, valeur, nbJoueurs, joueursX, joueursY);
+        return new EtatJeu(labyrinthe, liste);
     }
 
-    int index(int x, int y) {
-        return y * largeur + x;
+    Labyrinthe getLabyrinthe() {
+        return labyrinthe;
     }
 
-    boolean dansBornes(int x, int y) {
-        return x >= 0 && x < largeur && y >= 0 && y < hauteur;
+    int getNbJoueurs() {
+        return joueurs.length;
     }
 
-    boolean estMur(int x, int y) {
-        return type[index(x, y)] == TypeCase.MUR;
-    }
-
-    @Override
-    public String toString() {
-        return "EtatJeu{" +
-            "largeur=" + largeur +
-            ", hauteur=" + hauteur +
-            ", nbJoueurs=" + nbJoueurs +
-            ", joueursX=" + Arrays.toString(joueursX) +
-            ", joueursY=" + Arrays.toString(joueursY) +
-            '}';
+    Joueur getJoueur(int id) {
+        return joueurs[id];
     }
 }

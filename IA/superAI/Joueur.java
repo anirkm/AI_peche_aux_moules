4a129c80b86ba8f96593a8f48127474ce74ebb2a
KARAMI Anir <ak@revolik.com>
2025-12-02 11:25:02 +0100

    ajout de classe Joueur

---
+++ IA/superAI/Joueur.java
@@ -0,0 +1,60 @@
+package superAI;
+
+class Joueur {
+    private final int id;
+    private int x;
+    private int y;
+    private int bonusSaut;
+    private int bonusTroisPas;
+
+    Joueur(int id, int x, int y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getBonusSaut() {
+        return bonusSaut;
+    }
+
+    int getBonusTroisPas() {
+        return bonusTroisPas;
+    }
+
+    void ajouterBonusSaut() {
+        bonusSaut++;
+    }
+
+    void ajouterBonusTroisPas() {
+        bonusTroisPas++;
+    }
+
+    void consommerBonusSaut() {
+        if (bonusSaut > 0) {
+            bonusSaut--;
+        }
+    }
+
+    void consommerBonusTroisPas() {
+        if (bonusTroisPas > 0) {
+            bonusTroisPas--;
+        }
+    }
+}
